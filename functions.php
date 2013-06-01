f3e8cf92d7df55d96df9a812fcd5bddc067b4929
King Sidharth <king@kingsidharth.com>
2013-06-01 18:30:04 +0530

    Add compass, make things work. CSS base is ready.

--- functions.php
+++ functions.php
@@ -108,17 +108,11 @@ add_action( 'widgets_init', 'anamorhpic_widgets_init' );
 function anamorhpic_scripts() {
 	wp_enqueue_style( 'anamorhpic-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'anamorhpic-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'anamorhpic-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'anamorhpic-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'anamorhpic_scripts' );
 
