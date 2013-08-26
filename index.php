98d6584695fdfb21269eed8470220c0c7c5f4be4
King Sidharth <king@kingsidharth.com>
2013-08-26 20:00:50 +0530

    New front page

--- index.php
+++ index.php
@@ -29,31 +29,36 @@ get_header(); ?>
             foreach( $myposts as $post ) : setup_postdata($post); 
               $meta  = get_post_meta($post->ID);
               anamorphic_post_data($meta);
+              $book_title = get_the_title() . ' by ' . get_array_list($authors);
             ?>
 
               --><li class="post_item_wrap grid__item one-half">
-                   <a class="post_item" href="<?php the_permalink(); ?>">
-                   <img src="<?php echo $main_image; ?>" title="<?php echo $extended_title;?>" alt="<?php echo $extended_title;?>"/>
-                     <h2 class="entry-title"><?php the_title(); ?></h2>
-                     <div class="entry-meta">
+                <a class="post_item" href="<?php the_permalink(); ?>" title="Review of <?php the_title(); ?>">
+                   <img src="<?php echo $main_image; ?>" 
+                     alt="<?php the_title(); ?> by <?php the_array_list($authors); ?>"/>
+                   <h2 class="entry-title"><?php the_title(); ?></h2>
+                   <div class="entry-meta">
                      <?php 
-                        if($authors) {
-                          echo "<span class=author>by ";
-                          the_array_list($authors);
-                          echo "</span>";
-                        }
-                        if($rating){ 
-                          echo '<span class="rating">';
-                          anamorhpic_rating_to_star($rating);
-                          echo '</span>';
-                        } ?>
+                      if($authors) {
+                        echo "<span class=author><em>by</em> <strong>";
+                        the_array_list($authors);
+                        echo "</strong></span>";
+                      }
+                      if($rating){ 
+                        echo '<span class="rating">';
+                        anamorhpic_rating_to_star($rating);
+                        echo '</span>';
+                      } ?>
                      <span class="entry-date"><?php echo get_the_date(); ?></span>              
                    </div><!--.entry-meta-->
                  </a>
                </li><!--
-            <?php endforeach; ?>
-          --></ul>
-        </div><!-- #latest_films.post_list
+               <?php endforeach; ?>
+               --><li class="grid__item gamma one-whole">
+                 <a href="/category/book/">See all book reviews <i class="icon icon-chevron-right"></i></a>
+               </li>
+            </ul>
+          </div><!-- #latest_films.post_list
 
         --><div class="grid__item one-half post_list palm-one-whole">
           <h2 class="group-title grid__item one-whole">Latest Film Reviews</h2>
@@ -80,7 +85,10 @@ get_header(); ?>
                 </div><!--.entry-meta-->
               </a></li><!--
             <?php endforeach; ?>
-          --></ul>
+              --><li class="grid__item gamma one-whole">
+                 <a href="/category/film/">See all film reviews <i class="icon icon-chevron-right"></i></a>
+               </li>
+          </ul>
         </div> 
 
       </div><!-- #latest -->
