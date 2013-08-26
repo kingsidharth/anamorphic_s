6c8df09d6b4898ed0442851a9ca3d321680f3dfe
King Sidharth <king@kingsidharth.com>
2013-08-26 20:37:30 +0530

    Don't load GA if logged in

--- footer.php
+++ footer.php
@@ -38,6 +38,7 @@
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
   </script>
+  <?php if (!is_user_logged_in()) { ?>
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -48,5 +49,6 @@
     ga('send', 'pageview');
 
   </script>
+  <?php } ?>
 </body>
 </html>
