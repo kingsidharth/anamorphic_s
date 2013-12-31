29605af2d1f2605cb4e739e03a7e4cd171393f7d
King Sidharth <king@kingsidharth.com>
2013-12-31 13:31:37 +0530

    Separate Data

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@
 # INCLUDES & IMPORTS
 # ====================
 include('inc/helper.php');
+include('inc/data.php');
 
 // Register the Main Menu
 if ( ! function_exists( 'anamorhpic_setup' ) ) :
@@ -27,132 +28,6 @@ function anamorhpic_scripts() {
   wp_enqueue_style( 'anamorhpic-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'anamorhpic_scripts' );
-
-
-# ================================ 
-# TAXONOMIES & POST DATA 
-# ================================
-
-# REGISTER CUSTOM TAXONOMIES
-
-// Authors (for books)
-function anamorphic_authors_taxnomy()  {
-	$labels = array(
-		'name'                       => 'Authors',
-		'singular_name'              => 'Author',
-		'menu_name'                  => 'Authors',
-		'all_items'                  => 'All Authors',
-		'new_item_name'              => 'New Author',
-		'add_new_item'               => 'Add A New Author',
-		'edit_item'                  => 'Edit Author',
-		'update_item'                => 'Update Author',
-		'separate_items_with_commas' => 'Separate authors with commas',
-		'search_items'               => 'Search authors...',
-		'add_or_remove_items'        => 'Add or remove Authors',
-		'choose_from_most_used'      => 'Choose from the author',
-	);
-
-	$rewrite = array(
-		'slug'                       => 'authors',
-		'with_front'                 => true,
-		'hierarchical'               => false,
-	);
-
-	$args = array(
-		'labels'                     => $labels,
-		'hierarchical'               => false,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => false,
-		'rewrite'                    => $rewrite,
-	);
-
-	register_taxonomy( 'authors', 'post', $args );
-}
-
-add_action( 'init', 'anamorphic_authors_taxnomy', 0 );
-
-
-// Genre (common)
-function anamorphic_genre()  {
-	$labels = array(
-		'name'                       => 'Genre',
-		'singular_name'              => 'Genre',
-		'menu_name'                  => 'Genre',
-		'all_items'                  => 'All Genre',
-		'new_item_name'              => 'New Genre',
-		'add_new_item'               => 'Add A New Genre',
-		'edit_item'                  => 'Edit Genre',
-		'update_item'                => 'Update Genre',
-		'separate_items_with_commas' => 'Separate genre with commas',
-		'search_items'               => 'Search genre...',
-		'add_or_remove_items'        => 'Add or remove Genre',
-		'choose_from_most_used'      => 'Choose from the genre',
-	);
-
-	$rewrite = array(
-		'slug'                       => 'genre',
-		'with_front'                 => true,
-		'hierarchical'               => false,
-	);
-
-	$args = array(
-		'labels'                     => $labels,
-		'hierarchical'               => false,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => false,
-		'rewrite'                    => $rewrite,
-	);
-
-	register_taxonomy( 'genre', 'post', $args );
-}
-
-add_action( 'init', 'anamorphic_genre', 0 );
-
-
-// Actor(s)
-function anamorphic_actor()  {
-	$labels = array(
-		'name'                       => 'Actors',
-		'singular_name'              => 'Actor',
-		'menu_name'                  => 'Actors',
-		'all_items'                  => 'All Actors',
-		'new_item_name'              => 'New Actor',
-		'add_new_item'               => 'Add A New Actor',
-		'edit_item'                  => 'Edit Actor Details',
-		'update_item'                => 'Update Actor Details',
-		'separate_items_with_commas' => 'Separate actors with commas',
-		'search_items'               => 'Search actor...',
-		'add_or_remove_items'        => 'Add or remove Actor(s)',
-		'choose_from_most_used'      => 'Choose from the Actor',
-	);
-
-	$rewrite = array(
-		'slug'                       => 'actor',
-		'with_front'                 => true,
-		'hierarchical'               => false,
-	);
-
-	$args = array(
-		'labels'                     => $labels,
-		'hierarchical'               => false,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => false,
-		'rewrite'                    => $rewrite,
-	);
-
-	register_taxonomy( 'actor', 'post', $args );
-}
-
-add_action( 'init', 'anamorphic_actor', 0 );
 
 
 // Output List of Schema
