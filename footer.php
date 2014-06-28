fe608f77ffe2d736f384900e20e61a5e4463966a
King Sidharth <king.sidharth@yahoo.com>
2014-06-28 18:15:32 +0530

    Update Footer

--- footer.php
+++ footer.php
@@ -4,45 +4,30 @@
  */
 ?>
 
-<?php if(!is_front_page()) { ?>
- <div id="promo_area" class="grid">
-    <div class="page">
-      <div id="promo">
-        <div class="grid__item one-whole"><hr/></div>
-      
-        <div id="subscribe" class="grid__item one-third palm-one-whole">
-          <h4><i class="icon icon-check"> </i>Subscribe</h4>
-          <p>Liked what you read? You might want to subscribe to the updates:</p>
-          <ul class="nav nav--stacked nav--button nav--button-green">
-            <li><a class="fb-like" data-href="https://www.facebook.com/anamorphic.in" data-width="300" data-layout="button_count" data-show-faces="true"> Like on Facebook</a></li>
-            <li><a href="http://feeds.feedburner.com/anamorphic"><i class="icon icon-rss"> </i> RSS Feed</a></li>
-            <li><a href="https://www.goodreads.com/kingsidharth"><i class="icon icon-book"></i> Follow on Goodreads</a></li>
-          </ul>
-        </div><!-- #subscribe
-        --><div id="get_reviewed" class="grid__item one-third palm-one-whole">
-          <h4>Get Reviewed</h4>
-          <p>Want me to review your book/film? Email me: king(a)kingsidharth.com</p>
-        </div><!-- #get_reviewed
-
-        --><div id="elsewhere" class="grid__item one-third palm-one-whole">
-          <ul class="nav nav--stacked nav--button nav--button-red">
-            <li><a href="http://bit.ly/anatwitks"><i class="icon icon-twitter"> </i>@kingsidharth</a></li>
-            <li><a href="https://plus.google.com/100630005600562803456?rel=author" rel="author"><i class="icon icon-google-plus"> </i>Add on Google+</a></li>
-            <li><a href="http://bit.ly/anamorfbks"><i class="icon icon-facebook"> </i> Follow on Facebook</a></li>
-            <!--<li><a href=""><i class="icon icon-"> </i></a></li>-->
-          </ul>
-        </div><!-- #elsewhere -->
-        
-      </div><!-- #promo -->
-    </div>
-  </div><!-- #promo_area -->
-<?php } ?>
-
   <div id="footer_area" class="grid">
     <div class="page">
-      <footer role="contentinfo">
-        <p class="grid__item one-whole right">
+      <footer role="contentinfo" class="desk-right center">
+        <div class="grid__item one-whole"><hr/></div>
+
+        <nav class="grid__item one-whole full-bottom-margin" id="footer_navigation">
+        <ul class="nav nav--spaced">
+          <li><a href="/about"><i class="icon icon-disc green"></i> About Anamorphic</a></li>
+          <li class="separator"> | </li>
+          <li><a href="/">Goodreads</a></li>
+          <li class="separator"> | </li>
+          <li><a href="/"><i class="icon icon-facebook"></i> Facebook</a></li>
+          <li><a href="/"><i class="icon icon-rss"></i> Feed</a></li>
+        </ul>
+        </nav>
+
+        <p class="grid__item one-whole">
+        Want me to review a book / film? <a href="">@kingsidharth</a> 
+        or king(at)kingsidharth.com.
+        </p>
+
+        <p class="grid__item one-whole zeta">
           &copy; <?php echo date("Y"); ?> 
+          <!-- TODO: Add Google Plus Authorship -->
         </p>
       </footer>
     </div>
